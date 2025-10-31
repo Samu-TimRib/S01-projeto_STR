b9bc5a7d7ddb7ef6a87a0539f951f23058748e7e
Samu <92657957+Samu-TimRib@users.noreply.github.com>
2025-10-30 22:55:34 -0300

    Refactor Hunter classes from Java to JavaScript

--- JavaScript/Ex4.js
+++ JavaScript/Ex4.js
@@ -1,109 +1,43 @@
-import java.util.*;
+// Classe base Hunter
+class Hunter {
+  constructor(nome, localizacao, idade) {
+    this.nome = nome;
+    this.localizacao = localizacao;
+    this.idade = idade;
+  }
 
-// Interface que define o comportamento rastreável
-interface IRastreavel {
-    String rastrearLocal(double latitude, double longitude);
-}
-
-// Classe base abstrata
-abstract class Hunter implements IRastreavel {
-    protected String nome;
-    protected int idade;
-    protected String localizacao;
-
-    public Hunter(String nome, String localizacao, int idade) {
-        this.nome = nome;
-        this.localizacao = localizacao;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    // Hash e equals garantem que Hunters duplicados (mesmo nome) não sejam adicionados ao Set
-    @Override
-    public int hashCode() {
-        return Objects.hash(nome);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Hunter other = (Hunter) obj;
-        return Objects.equals(nome, other.nome);
-    }
+  rastrearLocal(latitude, longitude) {
+    console.log(`${this.nome} rastreou o local em (${latitude}, ${longitude})`);
+  }
 }
 
 // Subclasse Especialista
 class Especialista extends Hunter {
-    protected String habilidadeNen;
+  constructor(nome, habilidadeNen, localizacao, idade) {
+    super(nome, localizacao, idade);
+    this.habilidadeNen = habilidadeNen;
+  }
 
-    public Especialista(String nome, String habilidade, String local, int idade) {
-        super(nome, local, idade);
-        this.habilidadeNen = habilidade;
-    }
-
-    @Override
-    public String rastrearLocal(double latitude, double longitude) {
-        this.localizacao = "Lat: " + latitude + ", Long: " + longitude;
-        return "Especialista " + nome + " usando " + habilidadeNen + " rastreou local: " + localizacao;
-    }
+  rastrearLocal(latitude, longitude) {
+    console.log(`Especialista ${this.nome} usou ${this.habilidadeNen} para rastrear o local (${latitude}, ${longitude})`);
+  }
 }
 
 // Subclasse Manipulador
 class Manipulador extends Hunter {
-    protected Hunter alvoAtual;
+  constructor(nome, alvo, localizacao, idade) {
+    super(nome, localizacao, idade);
+    this.alvo = alvo;
+  }
 
-    public Manipulador(String nome, Hunter alvo, String local, int idade) {
-        super(nome, local, idade);
-        this.alvoAtual = alvo;
-    }
-
-    @Override
-    public String rastrearLocal(double latitude, double longitude) {
-        this.localizacao = "Lat: " + latitude + ", Long: " + longitude;
-        return "Manipulador " + nome + " rastreou " + (alvoAtual != null ? alvoAtual.getNome() : "ninguém") +
-               " em " + localizacao;
-    }
+  rastrearLocal(latitude, longitude) {
+    console.log(`Manipulador ${this.nome} está rastreando ${this.alvo} em (${latitude}, ${longitude})`);
+  }
 }
 
-// Classe Batalhao
-class Batalhao {
-    private Set<Hunter> hunters;
+// Teste
+const e1 = new Especialista("Gon", "Jajanken", "Yorknew", 14);
+const m1 = new Manipulador("Killua", "Gon", "Yorknew", 14);
 
-    public Batalhao() {
-        hunters = new HashSet<>();
-    }
-
-    public void adicionarHunter(Hunter h) {
-        hunters.add(h);
-    }
-
-    public int getNumHunters() {
-        return hunters.size();
-    }
-
-    public void iniciarRastreamento(double lat, double lon) {
-        for (Hunter h : hunters) {
-            System.out.println(h.rastrearLocal(lat, lon));
-        }
-    }
-}
-
-// Programa principal para teste
-public class Main {
-    public static void main(String[] args) {
-        Especialista e1 = new Especialista("Gon", "Jajanken", "Yorknew", 14);
-        Manipulador m1 = new Manipulador("Killua", e1, "Yorknew", 14);
-
-        Batalhao b = new Batalhao();
-        b.adicionarHunter(e1);
-        b.adicionarHunter(m1);
-        b.adicionarHunter(e1); // não será adicionado novamente
-
-        System.out.println("Total de Hunters no Batalhão: " + b.getNumHunters());
-        b.iniciarRastreamento(-3.14, -60.02);
-    }
-}
+e1.rastrearLocal(-3.14, -60.02);
+m1.rastrearLocal(-6.98, -12);
